93a8c9d50211985ae72c7166fe1e9709405dcbf7
ElasticBottle <winstonyeo99@yahoo.com>
2025-08-18 08:39:51 +0100

    chore: fix custom tanstack router link

--- apps/www/src/components/fancy-link.tsx
+++ apps/www/src/components/fancy-link.tsx
@@ -1,12 +1,13 @@
 import { cn } from "@rectangular-labs/ui/utils/cn";
-import { Link, type LinkComponent } from "@tanstack/react-router";
+import { createLink } from "@tanstack/react-router";
 
 export const FancyLinkVariant = () =>
   "relative h-full transform-gpu py-5 text-xl transition-colors will-change-transform before:absolute before:bottom-0 before:left-0 before:h-0.5 before:w-full before:origin-left before:scale-x-0 before:transform before:bg-current before:transition-transform before:duration-300 before:will-change-transform hover:before:scale-x-100 data-[active=true]:bg-sidebar data-[active=true]:font-bold";
 
-export function FancyLink(
-  args: Parameters<LinkComponent<"a">>[0] & { children: React.ReactNode },
-) {
-  const { className, ...rest } = args;
-  return <Link className={cn(FancyLinkVariant(), className)} {...rest} />;
-}
+interface BasicLinkProps extends React.ComponentPropsWithRef<"a"> {}
+
+const BasicLinkComponent = ({ className, ...props }: BasicLinkProps) => {
+  return <a {...props} className={cn(FancyLinkVariant(), className)} />;
+};
+
+export const FancyLink = createLink(BasicLinkComponent);
